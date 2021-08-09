0e2c2efe6452deb2bcf4d03b39630c97853f83bf
Jesper Laurell <jesper.laurell1@gmail.com>
2021-08-09 20:55:51 +0200

    11 wip

--- projecteuler/euler11.py
+++ projecteuler/euler11.py
@@ -1,3 +1,44 @@
+from numpy.core.fromnumeric import prod
+import numpy as np
+
+def get_max(mat):
+    biggest = 0
+    for row in mat:
+        i = 0
+        while i < 17:
+            window = row[i:i+4]
+            p = prod(window)
+
+            if p > biggest:
+                biggest = p
+            
+            i += 1
+
+    return biggest
+
+def get_diag_max(mat):
+    biggest = 0
+    for i in range(17):
+        for j in range(17):
+            p = 1
+            p_inv = 1
+            for k in range(4):
+                p *= mat[i + k][j + k]
+
+            if p > biggest:
+                biggest = p
+
+    for i in range(19, 4, -1):
+        for j in range(19, 4, -1):
+            p = 1
+            for k in range(4):
+                p *= mat[i - k][j - k]
+
+            if p > biggest:
+                biggest = p
+    return biggest
+
+# parsing
 with open('euler11input.txt', 'r') as f:
     f_contents = f.readlines()
 
@@ -5,7 +46,18 @@ f_contents = [l.strip() for l in f_contents]
 
 mat = []
 for l in f_contents:
-    for n in l.split(' '):
-        print(n)
+    m = [int(a) for a in l.split(' ')]
+    mat.append(m)
 
-# print(f_contents)
+# horizontally
+hor = get_max(mat)
+
+# vertically
+mat_transpose = np.array(mat).transpose()
+vert = get_max(mat_transpose)
+
+# diagonally
+diag = get_diag_max(mat)
+
+
+print(hor, vert, diag)
